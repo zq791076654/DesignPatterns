fe84cb474489b73c3cc855327e021509f31f0d35
qiangz2 <qiangz2@wicresoft.com>
2019-08-06 09:25:41 +0800

    add Simple Factory Pattern

---
+++ SimpleFactoryPattern/FruitSimpleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    public static class FruitSimpleFactory
+    {
+        public static Fruit CreateFruit(string fruitType)
+        {
+            switch (fruitType)
+            {
+                case "Apple":
+                    return new Apple();
+                case "Orange":
+                    return new Orange();
+                case "Banana":
+                    return new Banana();
+                default:
+                    throw new ArgumentException("Not Found Fruit!");
+            }
+        }
+    }
+}
---
+++ SimpleFactoryPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 不使用工厂模式
+            //Apple apple = new Apple();
+            //apple.Print();
+            //Orange orange = new Orange();
+            //orange.Print();
+            //Banana banana = new Banana();
+            //banana.Print();
+            #endregion
+
+            #region 使用简单工厂模式
+            Fruit fruitApple = FruitSimpleFactory.CreateFruit("Apple");
+            fruitApple.Print();
+            Fruit fruitOrange = FruitSimpleFactory.CreateFruit("Orange");
+            fruitOrange.Print();
+            Fruit fruitBanana = FruitSimpleFactory.CreateFruit("Banana");
+            fruitBanana.Print();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
